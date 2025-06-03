a9dce604899fd9b1e0eaa7116a20b1646103bfd2
Weatherserin <erin.weathers@student.uagc.edu>
2025-06-02 23:26:57 -0700

    Update App.js

--- App.js
+++ App.js
@@ -8,17 +8,34 @@ import About from './components/About';
 import './styles/App.css';
 
 const App = () => (
-  <Router>
-    <Menu />
-    <div className="main-content">
-      <Routes>
-        <Route path="/streamlist" element={<StreamList />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </div>
-  </Router>
+  <div
+    style={{
+      minHeight: "100vh",
+      backgroundImage: "url('/assets/TEXTURE_3.jpg')",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundAttachment: "fixed",
+    }}
+  >
+    {/* Example: Add a logo or banner at the top */}
+    <img 
+      src="/assets/logo.svg" 
+      alt="Description" 
+      style={{ width: '300px', margin: '2rem auto', display: 'block' }} 
+      />
+    <Router>
+      <Menu />
+      <div className="main-content">
+        <Routes>
+          <Route path="/streamlist" element={<StreamList />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  </div>
 );
 
 export default App;
